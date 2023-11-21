1245f8f21820e3698c06a386be6668e20cddfb52
EduardoMatheus96 <eduardomatheusuesc@gmail.com>
2023-11-21 20:29:43 -0300

    Terminando a parte visual da pagina bookListPage

--- leitor_ebooks/lib/src/pages/book_list/book_list_page.dart
+++ leitor_ebooks/lib/src/pages/book_list/book_list_page.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:leitor_ebooks/src/api/models/book_model.dart';
 import 'package:leitor_ebooks/src/services/book_service.dart';
+import 'package:leitor_ebooks/src/widgets/book_tile.dart';
 
 class BookListPage extends StatefulWidget {
   final BookService bookService;
@@ -26,6 +27,64 @@ class _BookListPageState extends State<BookListPage> {
     return Scaffold(
       appBar: AppBar(
         title: const Text('Lista de Livros'),
+      ),
+      body: FutureBuilder(
+        future: _booksFuture,
+        builder: (context, snapshot) {
+          if (snapshot.connectionState == ConnectionState.waiting) {
+            return const Center(
+              child: CircularProgressIndicator(),
+            );
+          } else if (snapshot.hasError) {
+            return Center(
+              child: Text('Erro: ${snapshot.error}'),
+            );
+          } else {
+            final books = snapshot.data!;
+            return Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                Row(
+                  children: [
+                    ElevatedButton(
+                      onPressed: () {
+                        // Navegar para lista de livros ...
+                      },
+                      child: const Text('Livros'),
+                    ),
+                    const SizedBox(width: 80),
+                    ElevatedButton(
+                      onPressed: () {
+                        // Navegar para lista de favoritos,
+                      },
+                      child: const Text('Favoritos'),
+                    ),
+                  ],
+                ),
+                Expanded(
+                  child: GridView.builder(
+                    gridDelegate:
+                        const SliverGridDelegateWithFixedCrossAxisCount(
+                      crossAxisCount: 2,
+                    ),
+                    itemCount: books.length,
+                    itemBuilder: (context, index) {
+                      final book = books[index];
+                      return BookTile(
+                        book: book,
+                        isFavorite:
+                            false, // Falta criar o metodo para definir isso ainda
+                        onFavoriteTap: () {
+                          // Implementar logica para favoritar um livro ...
+                        },
+                      );
+                    },
+                  ),
+                ),
+              ],
+            );
+          }
+        },
       ),
     );
   }
---
+++ leitor_ebooks/lib/src/pages/favorites/favorites_page.dart
@@ -0,0 +1,17 @@
+// No arquivo src/pages/favorites/favorites_page.dart
+
+import 'package:flutter/material.dart';
+
+class FavoritesPage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Favoritos'),
+      ),
+      body: Center(
+        child: Text('Página de Favoritos'),
+      ),
+    );
+  }
+}
