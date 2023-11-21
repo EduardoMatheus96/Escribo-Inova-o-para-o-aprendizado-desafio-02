b79f1cac02fea3b34897cf6ce4cd8611f0701b28
EduardoMatheus96 <eduardomatheusuesc@gmail.com>
2023-11-21 11:12:52 -0300

    Terminando a book_tile.dart

--- leitor_ebooks/lib/src/pages/book_list/book_list_page.dart
+++ leitor_ebooks/lib/src/pages/book_list/book_list_page.dart
@@ -1,15 +1,32 @@
 import 'package:flutter/material.dart';
+import 'package:leitor_ebooks/src/api/models/book_model.dart';
+import 'package:leitor_ebooks/src/services/book_service.dart';
 
 class BookListPage extends StatefulWidget {
-  const BookListPage({super.key});
+  final BookService bookService;
+
+  const BookListPage({super.key, required this.bookService});
 
   @override
   State<BookListPage> createState() => _BookListPageState();
 }
 
 class _BookListPageState extends State<BookListPage> {
+  late Future<List<Book>> _booksFuture;
+
+  @override
+  void initState() {
+    // TODO: implement initState
+    super.initState();
+    _booksFuture = widget.bookService.getBooks();
+  }
+
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Lista de Livros'),
+      ),
+    );
   }
 }
